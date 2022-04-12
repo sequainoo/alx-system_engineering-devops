acec3db2516fcc52007f898ddec280226e41c501
sequainoo <wazatoyung@gmail.com>
2022-04-12 13:53:32 -0700

    fix import: gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,29 +2,29 @@
 """Makes an API request to return the todo list progress of an employee.
 Takes in an argument asthe employee ID.
 """
+from requests import get
 import sys
 
-from requests import get
+if __name__ == '__main__':
+    endpoint = 'https://jsonplaceholder.typicode.com/todos'
+    response = get(endpoint)
+    data = response.json()
+    id_ = int(sys.argv[1])
 
-endpoint = 'https://jsonplaceholder.typicode.com/todos'
-response = get(endpoint)
-data = response.json()
-id_ = int(sys.argv[1])
+    # get user name
+    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(id_))
+    name = name.json().get('name')
 
-# get user name
-name = get('https://jsonplaceholder.typicode.com/users/{}'.format(id_))
-name = name.json()['name']
+    # filter todos for employee id
+    todos = [todo for todo in data if todo.get('userId') == id_]
+    total = len(todos)
+    completed = 0
+    titles = ''
 
-# filter todos for employee id
-todos = [todo for todo in data if todo['userId'] == id_]
-total = len(todos)
-completed = 0
-titles = ''
+    for todo in todos:
+        if todo.get('completed'):
+            completed += 1
+            titles += '\t ' + todo.get('title') + '\n'
 
-for todo in todos:
-    if todo['completed']:
-        completed += 1
-        titles += '\t ' + todo['title'] + '\n'
-
-print('Employee {} is done with tasks({}/{}):'.format(name, completed, total))
-print(titles, end='')
+    print('Employee {} is done with tasks({}/{}):'.format(name, completed, total))
+    print(titles, end='')
