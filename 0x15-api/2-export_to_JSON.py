27e1a1704cc1561644a6877a8db646056ce2fe88
sequainoo <wazatoyung@gmail.com>
2022-04-12 15:30:55 -0700

    add 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,6 +2,7 @@
 """Makes an API request to return the todo list progress of an employee.
 Takes in an argument asthe employee ID.
 """
+import json
 from requests import get
 import sys
 
@@ -13,18 +14,20 @@ if __name__ == '__main__':
 
     # get user name
     name = get('https://jsonplaceholder.typicode.com/users/{}'.format(id_))
-    name = name.json().get('name')
+    name = name.json().get('username')
 
     # filter todos for employee id
     todos = [todo for todo in data if todo.get('userId') == id_]
-    total = len(todos)
-    done = 0
-    titles = ''
-
+    dict_ = {}
+    list_ = []
     for todo in todos:
-        if todo.get('completed'):
-            done += 1
-            titles += '\t ' + todo.get('title') + '\n'
+        d_ = {'task': todo.get('title'),
+              'completed': todo.get('completed'),
+              'username': name}
+        list_.append(d_)
+    dict_.update({'{}'.format(id_): list_})
 
-    print('Employee {} is done with tasks({}/{}):'.format(name, done, total))
-    print(titles, end='')
+    # json
+    filename = '{}.json'.format(id_)
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(dict_, f)
